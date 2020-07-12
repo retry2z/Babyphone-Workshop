92c412105ca1ab25b85401176b39312f71c9208c
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-07-12 15:32:12 +0300

    feat/ implement main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import style from './App.module.css';
-import Navigation from './components/navigation/navigation';
-import Aside from './components/aside/aside';
+import Navigation from './components/core/navigation/navigation';
+import Aside from './components/core/aside/aside';
+import Main from './components/core/main/main';
 
 const App = () => {
   return (
     <div className={style.app}>
       <Navigation />
-      <Aside />
+
+      <div className={style.container}>
+        <Aside />
+        <Main />
+      </div>
+
+
     </div>
   );
 }
