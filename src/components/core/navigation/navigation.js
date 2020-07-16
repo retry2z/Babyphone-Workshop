61c80ca6f35c1527ea9b80e2f1c5483245a424e4
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-07-16 07:47:15 +0300

    feat/ implement logo component

--- src/components/core/navigation/navigation.js
+++ src/components/core/navigation/navigation.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import style from './navigation.module.css';
 import Link from '../link/link';
+import Logo from '../logo/logo';
+import logo from '../../../images/white-origami-bird.png';
 
 const Navigation = () => {
     return (
         <header className={style.navigation}>
             <ul>
+                <Logo href={logo} title="Logo" />
                 <Link href="#" title="Going to 1" />
                 <Link href="#" title="Going to 2" />
                 <Link href="#" title="Going to 3" />
