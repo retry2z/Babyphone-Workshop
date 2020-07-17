3d00d514c67563955d16c133f53d9802557a8a5c
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-07-17 11:32:04 +0300

    feat/ implement product service

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import style from './product.module.css'
 import Card from './card/card';
+import productService from './product-service';
 
-const Product = () => {
-    return (
-        <div className={style.container}>
-            <Card author="test author" description="default desc"/>
-            
-        </div>
-    )
+class Product extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        }
+    }
+
+    async componentDidMount() {
+        const { data } = await productService.list();
+        this.setState({
+            data,
+        });
+    }
+
+    renderOrigamis() {
+        const { data } = this.state
+
+        return data.map(doc => {
+            return (
+                <Card key={doc.id} author={doc.author} description={doc.description} />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className={style.container} >
+                {this.renderOrigamis()}
+            </div>
+        )
+    }
 }
 
 export default Product
