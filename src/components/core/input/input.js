69cc4b98a6fdb6b7b49af668d06a36ea872beb6a
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-07-23 15:38:03 +0300

    -

--- src/components/core/input/input.js
+++ src/components/core/input/input.js
@@ -2,10 +2,12 @@ import React from 'react'
 import style from './input.module.css'
 
 const InputField = ({ type, placeholder, onChange, value }) => {
+    const id = Math.random() / 1000;
+
     return (
         <div className={style.input}>
-            <input type={type} onChange={onChange} value={value} />
-            <label htmlFor={type}>{placeholder}</label>
+            <input id={id} type={type} onChange={onChange} value={value} />
+            <label htmlFor={id}>{placeholder}</label>
         </div>
     )
 }
