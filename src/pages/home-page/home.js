3a0ece12a24e00d97b5c9ea569dc3a1390f39999
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-08-03 16:21:48 +0300

    feat/search-room

--- src/pages/home-page/home.js
+++ src/pages/home-page/home.js
@@ -34,8 +34,28 @@ class Home extends React.Component {
         });
     }
 
-    submitHandler(data) {
-        console.log(data);
+    search = (arr, data) => {
+        return arr.filter(item => {
+            const name = item.data.title.toLowerCase();
+            const description = item.data.keyWords.join(' ').toLowerCase();
+            const search_data = data.toLowerCase() || '';
+
+            if (name.includes(search_data) || description.includes(search_data)) {
+                return item
+            } else {
+                return false
+            }
+        });
+    }
+
+
+    submitHandler = (data) => {
+        const result = this.search(this.state.data, data.search);
+
+        this.setState({
+            ...this.state,
+            data: result
+        })
     }
 
     render() {
